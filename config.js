9876e3b049a75ad6d521b392b8b11e182e51c9e2
LinhND <haixoay96@gmail.com>
2020-12-07 21:26:57 +0700

    update image

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "Gửi đến Dương",
+        "Gửi đến Dương xinh gái",
         "Dương thân mến",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
         "Hôm nay là sinh nhật cậu",  // 同上...
         "Linh chúc Dương tuổi mới",
@@ -34,7 +34,15 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/duong.jpg",
+        "Gửi đến Dương xinh gái": "./imgs/duong.jpg",
+        "Dương thân mến": "./imgs/duong1.jpg",
+        "Hôm nay là sinh nhật cậu":"./imgs/duong2.jpg",  // 同上...
+        "Linh chúc Dương tuổi mới":"./imgs/duong3.jpg",
+        "Sức khoẻ dồi dào":"./imgs/duong4.jpg",
+        "Gia đình luôn luôn sung túc":"./imgs/duong5.jpg",
+        "Thành công nối tiếp thành công":"./imgs/duong6.jpg",
+        "Sinh nhật vui vẻ ý nghĩa nha":"./imgs/duong7.jpg",
+        "-Linh-":"./imgs/linh.jpg",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
